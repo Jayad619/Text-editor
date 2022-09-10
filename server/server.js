e72ae93d2e99d13fe1ffd35296e69123eb2d961f
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-10 02:01:21 +0100

    still fixing

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + '/dist'));
 
 require('./routes/htmlRoutes')(app);
 
